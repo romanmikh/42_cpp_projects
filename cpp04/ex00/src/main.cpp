b2edfab36781274f16be48f14b10f11c09e06106
¨Roman <roman.mikhaylenko14@hotmail.com>
2025-03-15 11:43:43 +0000

    complete: cpp04

--- cpp04/ex00/src/main.cpp
+++ cpp04/ex00/src/main.cpp
@@ -17,56 +17,56 @@
 #include "../inc/WrongAnimal.hpp"
 #include "../inc/WrongCat.hpp"
 
-int main(void)
-{
-    const Animal* meta = new Animal();
-    const Animal* i = new Cat();
-    const Animal* j = new Dog();
-    std::cout << std::endl;
-
-    printStr("meta is a " + meta->getType(), "G");
-    printStr("i is a " + i->getType(), "R");
-    printStr("j is a " + j->getType(), "B");
-    std::cout << std::endl;
-
-    meta->makeSound();
-    i->makeSound();
-    j->makeSound();
-    std::cout << std::endl;
-
-    delete meta;
-    delete i;
-    delete j;
-
-    return 0;
-}
-
 // int main(void)
 // {
-//     const WrongAnimal* meta = new WrongAnimal();
-//     const WrongAnimal* i = new WrongCat();
+//     const Animal* meta = new Animal();
+//     const Animal* i = new Cat();
+//     const Animal* j = new Dog();
 //     std::cout << std::endl;
 
 //     printStr("meta is a " + meta->getType(), "G");
 //     printStr("i is a " + i->getType(), "R");
+//     printStr("j is a " + j->getType(), "B");
 //     std::cout << std::endl;
 
 //     meta->makeSound();
 //     i->makeSound();
+//     j->makeSound();
 //     std::cout << std::endl;
 
 //     delete meta;
 //     delete i;
-//     std::cout << std::endl << std::endl;
-
-//     printStr("BUT if we instantiate WrongCat without animal:", "R");
-
-//     const WrongCat* j = new WrongCat();
-//     j->makeSound();
-//     printStr("it doesn't inherit Animal's makeSound()", "R");
-//     std::cout << std::endl;
-
 //     delete j;
 
 //     return 0;
 // }
+
+int main(void)
+{
+    const WrongAnimal* meta = new WrongAnimal();
+    const WrongAnimal* i = new WrongCat();
+    std::cout << std::endl;
+
+    printStr("meta is a " + meta->getType(), "G");
+    printStr("i is a " + i->getType(), "R");
+    std::cout << std::endl;
+
+    meta->makeSound();
+    i->makeSound();
+    std::cout << std::endl;
+
+    delete meta;
+    delete i;
+    std::cout << std::endl << std::endl;
+
+    printStr("BUT if we instantiate WrongCat without animal:", "R");
+
+    const WrongCat* j = new WrongCat();
+    j->makeSound();
+    printStr("it doesn't inherit Animal's makeSound()", "R");
+    std::cout << std::endl;
+
+    delete j;
+
+    return 0;
+}
