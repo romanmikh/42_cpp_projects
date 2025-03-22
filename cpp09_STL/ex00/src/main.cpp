f2e5e33c73f70023833028f58fd5180289196d39
¨Roman <roman.mikhaylenko14@hotmail.com>
2025-03-22 16:03:38 +0000

    edit: cpp09

---
+++ cpp09_STL/ex00/src/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rocky <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 15:30:54 by rocky             #+#    #+#             */
+/*   Updated: 2024/08/26 17:31:01 by rocky            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/BitcoinExchange.hpp"
+#include "../inc/Utils.hpp"
+
+#define RESET	"\e[0m"
+#define RED		"\e[31m"
+#define GREEN	"\e[32m"
+#define YELLOW	"\e[33m"
+#define BLUE	"\e[34m"
+#define PURPLE	"\e[35m"
+#define CYAN	"\e[36m"
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        printStr("Usage: ./btc [filename]", "R");
+        return 1;
+    }
+
+    BitcoinExchange exchange;
+    exchange.readData(av[1]);
+    exchange.printFile(av[1]);
+    return 0;
+}
